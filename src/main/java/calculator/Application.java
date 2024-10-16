7bf6aad94be2df05f04829e392fd4d821254f3be
chan152 <chan152@naver.com>
2024-10-17 00:40:03 +0900

    Update Application.java
    사용자에게 입력 값 받기,결과 값 출력하기

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,5 +3,10 @@ package calculator;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+                System.out.println("덧셈할 문자열을 입력해 주세요 :");
+        String num = Console.readLine();
+
+
+        System.out.println("결과 : " + addnum);
     }
 }
